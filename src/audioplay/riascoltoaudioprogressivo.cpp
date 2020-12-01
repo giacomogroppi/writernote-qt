770e8792f4b8fa8ee657a1792a2e30ab4d7b4317
giacomogroppi <giamg01@gmail.com>
2020-12-01 15:51:55 +0100

    modificato:             src/audioplay/riascoltoaudioprogressivo.cpp

--- src/audioplay/riascoltoaudioprogressivo.cpp
+++ src/audioplay/riascoltoaudioprogressivo.cpp
@@ -11,28 +11,59 @@ void MainWindow::riascoltoaudioprogressivo(qint64 position){
     if(this->player->state() != QMediaPlayer::PlayingState)
         return;
 
-
-#ifdef STAMPA
+/*#ifdef STAMPA
     qDebug() << "position" << position;
-#endif
+#endif*/
 
-    int position_inlist = this->self->currenttitle.posizione_iniz.indexOf(position);
+    int position_inlist = this->self->currenttitle.posizione_iniz.indexOf(position/1000);
 
     if(position_inlist == -1) return;
 
-    //int lung = this->self->currenttitle.testinohtml[position_inlist].length();
+
 
     QString testoGrassetto = "<!DOCTYPE html><html><body><b>" + this->self->currenttitle.testinohtml[position_inlist] + "</b>";
-    testoGrassetto += self->currenttitle.testinohtml.last().mid(position_inlist,-1) + "</body></html>";
+    testoGrassetto += self->currenttitle.testinohtml.last().mid(position_inlist, -1) + "</body></html>";
 
     this->ui->textEdit->setHtml(testoGrassetto);
 
     this->ui->audioSlider->blockSignals(true);
 
-    if (this->player->duration() != 0)
-        this->ui->audioSlider->setValue(position/this->player->duration()*100);
+    qDebug() << "tempo -> " << this->player->duration();
+
+    this->ui->audioSlider->setValue((position*100)/this->player->duration());
 
     this->ui->audioSlider->blockSignals(false);
+
+
+    this->ui->statusBar->showMessage(tr("%1 second").arg(position / 1000));
 }
 
 #endif
+
+/*
+
+self.currentTime = int(position/1000)
+        print("Audio time: {}".format(self.currentTime))
+
+        if self.play_:
+            try:
+                position = self.currentTitleJSON['posizione_iniz'].index(str(self.currentTime))
+                print(self.currentTitleJSON['posizione_iniz'][position])
+            except:
+                ''' in caso la riga sia stata eliminata da data.py '''
+                return False
+
+            ''' next data structure '''
+            try:
+                lung = len(self.currentTitleJSON['testinohtml'][position])
+                testoGrassetto = '<!DOCTYPE html><html><body><b>' + self.currentTitleJSON['testinohtml'][position] + '</b>'
+                testoGrassetto += self.currentTitleJSON['testinohtml'][-1][lung:] + '</body></html>'
+                print(position)
+            except IndexError:
+                print("\nindexerror")
+                pass
+
+            self.editor.setHtml(testoGrassetto)
+
+
+*/
