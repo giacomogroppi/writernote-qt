9a7b73ce0665a910d873e838fcc7f72cc87efb86
Giacomo Groppi <giamg01@gmail.com>
2020-12-06 23:55:40 +0100

    print

--- src/style/print_action.cpp
+++ src/style/print_action.cpp
@@ -2,12 +2,15 @@
 #include "ui_mainwindow.h"
 
 #include <QPrintDialog>
+#include <QDialog>
+#include <QPrinter>
 
 /* stampa */
 void MainWindow::on_actionPrint_triggered()
 {
-    QPrintDialog *dialogprint = new QPrintDialog;
+    QPrinter *printer = new QPrinter;
+    QPrintDialog *dialogprint = new QPrintDialog(printer);
 
-    /*if (dialogprint->exec())
-        this->ui->textEdit->print(dialogprint->printer());*/
+    if (dialogprint->exec() == QDialog::Accepted)
+        this->ui->textEdit->print(printer);
 }
