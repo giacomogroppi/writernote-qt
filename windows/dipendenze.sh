ca081020b88487d7ce3f82b8414c1ccee0498c96
Giacomo Groppi <giamg01@gmail.com>
2021-01-05 09:50:56 +0100

    modificato:             windows/dipendenze.sh

--- windows/dipendenze.sh
+++ windows/dipendenze.sh
@@ -1,11 +1,11 @@
 pacman -S git
 pacman -S mingw-w64-x86_64-toolchain \
           mingw-w64-x86_64-cmake \
-          mingw-w64-x86_64-ninja \
           patch \
           mingw-w64-x86_64-cppunit \
           make
 
 
 pacman -S mingw-w64-x86_64-qt5 \
-          mingw-w64-x86_64-libzip
+          mingw-w64-x86_64-libzip \
+          mingw-w64-x86_64-glib2
