9e0d6dd4be3912dd4eeefb70388e66cbeef417bd
Giacomo Groppi <giamg01@gmail.com>
2021-01-12 19:32:35 +0100

    bug fixing for windows

---
+++ windows/compile.psm1
@@ -0,0 +1,23 @@
+rm build/writernote.exe
+rm -r build/audio
+rm -r build/bearer
+rm -r build/iconengines
+rm -r build/imageformats
+rm -r build/mediaservice
+rm -r build/platforms
+rm -r build/playlistformats
+rm -r build/printsupport
+rm -r build/styles
+rm -r build/translations
+rm build/Qt5Network.dll
+rm build/Qt5Multimedia.dll
+rm build/Qt5Gui.dll
+rm build/Qt5Core.dll
+rm build/Qt5PrintSupport.dll
+rm build/Qt5Svg.dll
+rm build/Qt5Widgets.dll
+
+qmake writernote.pro
+make
+
+windeployqt.exe ./build/writernote.exe --force
