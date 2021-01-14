71dd4c0faa125fc7bd481ef134d8f64dc7a8de7e
giacomogroppi <giamg01@gmail.com>
2021-01-14 11:32:50 +0100

    bug fix for windows

--- windows/compile.sh
+++ windows/compile.sh
@@ -4,6 +4,6 @@ rm build/*
 qmake writernote.pro
 make
 
-windeployqt.exe ./build/writernote.exe --force
-
 ldd ./build/writernote.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp "{}" ./build
+
+windeployqt.exe ./build/writernote.exe --force
