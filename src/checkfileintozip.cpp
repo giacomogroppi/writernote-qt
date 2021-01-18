01f36c435435da91a32fa4968f36c0e6668674bd
giacomogroppi <giamg01@gmail.com>
2021-01-18 17:10:26 +0100

    eliminato:              src/checkfileintozip.cpp 	eliminato:              src/moc_mainwindow.cpp

--- src/checkfileintozip.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef CHECK_FILE_INTO_ZIP_CPP
-#define CHECK_FILE_INTO_ZIP_CPP
-
-void checkfileintozip(const char *posizionezip, const char *nomedacercare){
-
-}
-
-#endif // CHECK_FILE_INTO_ZIP_CPP
