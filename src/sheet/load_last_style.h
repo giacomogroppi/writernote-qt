bb2d5d96a0fd874270ef2a67c3c31495387a54a5
giacomogroppi <giamg01@gmail.com>
2021-02-03 23:51:38 +0100

    Update load_last_style.h

--- src/sheet/load_last_style.h
+++ src/sheet/load_last_style.h
@@ -14,9 +14,9 @@ style_struct_S * load_default_drawing();
 /* the function save the default setting for drawing */
 void save_default_drawing(int *);
 
-void load_default(struct style_struct *);
+void load_default(style_struct *);
 
 /* the function is called by disegnofoglio */
-void load_default_onlystyle(struct style_struct_S *default_setting);
+void load_default_onlystyle(style_struct_S *default_setting);
 
 #endif // LOAD_LAST_STYLE_H
