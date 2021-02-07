8d61089efa4b40916f694f3277f307e9c1ec97c1
giacomogroppi <giamg01@gmail.com>
2021-02-07 11:14:47 +0100

    Update colortoint.cpp

--- src/datawrite/colortoint.cpp
+++ src/datawrite/colortoint.cpp
@@ -1,5 +1,5 @@
 #include "colortoint.h"
 
 void colortoint(QColor *color_, int *pointer){
-    color_->getRgb(&pointer[0], &pointer[1], &pointer[2]);
+    color_->getRgb(&pointer[0], &pointer[1], &pointer[2], &pointer[3]);
 }
