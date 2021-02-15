55e1d7d4ed733d9a564a33930a2bf4f7cce599f7
giacomogroppi <giamg01@gmail.com>
2021-02-15 09:55:31 +0100

    Update loadbinario.cpp

--- src/dataread/loadbinario.cpp
+++ src/dataread/loadbinario.cpp
@@ -6,6 +6,9 @@
 
 #include "../datawrite/source_read_ext.h"
 
+#define SOURCE_READ_ZIP_PRIVATE(x, y, z) if(zip_fread(x, y, z)==-1)
+#define SOURCE_READ_ZIP(x, y, z) check+=SOURCE_READ_ZIP_PRIVATE(x, y, z)
+
 /* la funzione gestisce la lettura del file binario */
 bool xmlstruct::loadbinario(zip_t *z){
     struct zip_stat st;
@@ -24,65 +27,72 @@ bool xmlstruct::loadbinario(zip_t *z){
     float temp;
     double variabiledouble;
 
-    /* x */
-    zip_fread(f, &lunghezza, sizeof(int));
+    /* x lenght*/
+    SOURCE_READ_ZIP(f, &lunghezza, sizeof(int));
+    //zip_fread(f, &lunghezza, sizeof(int));
 
 
     for(i=0; i < lunghezza; i++){
-        check += source_read_ext(f, &variabiledouble, sizeof(double));
+        SOURCE_READ_ZIP(f, &variabiledouble, sizeof(double));
+        //check += source_read_ext(f, &variabiledouble, sizeof(double));
         this->currenttitle->datatouch->x.append(variabiledouble);
     }
 
     /* y */
     for(i=0; i < lunghezza; i++){
-        check += source_read_ext(f, &variabiledouble, sizeof(double));
+        SOURCE_READ_ZIP(f, &variabiledouble, sizeof(double));
+        //check += source_read_ext(f, &variabiledouble, sizeof(double));
         this->currenttitle->datatouch->y.append(variabiledouble);
     }
 
     /* idtratto */
     for(i=0; i < lunghezza; i++){
-        check += source_read_ext(f, &valoretemp, sizeof(int));
+        SOURCE_READ_ZIP(f, &valoretemp, sizeof(int));
+        //check += source_read_ext(f, &valoretemp, sizeof(int));
         this->currenttitle->datatouch->idtratto.append(valoretemp);
     }
 
     /* pressure */
     for(i=0; i < lunghezza; i++){
-        check += source_read_ext(f, &temp, sizeof(float));
+        SOURCE_READ_ZIP(f, &temp, sizeof(float));
+        //check += source_read_ext(f, &temp, sizeof(float));
         this->currenttitle->datatouch->pressure.append(temp);
     }
 
     /* rotation */
     for(i=0; i < lunghezza; i++){
-        check += source_read_ext(f, &valoretemp, sizeof(int));
+        SOURCE_READ_ZIP(f, &valoretemp, sizeof(int));
+        //check += source_read_ext(f, &valoretemp, sizeof(int));
         this->currenttitle->datatouch->rotation.append(valoretemp);
     }
 
     /* colori */
     struct colore_s coloretemp;
     for(i = 0; i < lunghezza; i++){
-        /*check += source_read_ext(f, &coloretemp.colore[0], sizeof(int));
-        check += source_read_ext(f, &coloretemp.colore[1], sizeof(int));
-        check += source_read_ext(f, &coloretemp.colore[2], sizeof(int));
-        check += source_read_ext(f, &coloretemp.colore[2], sizeof(int));*/
-        source_read_ext(f, &coloretemp, sizeof(struct colore_s));
+        SOURCE_READ_ZIP(f, &cloretemp, sizeof(struct colore_s));
+        //source_read_ext(f, &coloretemp, sizeof(struct colore_s));
 
         currenttitle->datatouch->color.append(coloretemp);
     }
 
     /* carica la posizione dei testi */
     for(i=0; i < lunghezza; i++){
+        SOURCE_READ_ZIP(f, &valoretemp, sizeof(int));
         check += source_read_ext(f, &valoretemp, sizeof(int));
         this->currenttitle->datatouch->posizioneaudio.append(valoretemp);
     }
 
     /* posizione foglio */
-    check += source_read_ext(f, &lunghezza, sizeof(int));
+    SOURCE_READ_ZIP(f, &lunghezza, sizeof(int));
+    //check += source_read_ext(f, &lunghezza, sizeof(int));
     for(i=0; i < lunghezza; i++){
-        check += source_read_ext(f, &valoretemp, sizeof(int));
+        SOURCE_READ_ZIP(f, &valoretemp, sizeof(int));
+        //check += source_read_ext(f, &valoretemp, sizeof(int));
         currenttitle->datatouch->posizionefoglio.append(valoretemp);
     }
-
-    check += source_read_ext(f, &this->currenttitle->datatouch->zoom, sizeof(long double));
+    
+    SOURCE_READ_ZIP(f, &this->currenttitle->datatouch->zoom, sizeof(long double));
+    //check += source_read_ext(f, &this->currenttitle->datatouch->zoom, sizeof(long double));
 
     check += load_image(&currenttitle->datatouch->immagini, f);
 
