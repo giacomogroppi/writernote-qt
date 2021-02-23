28eb67dc03718bc87b52673bd037748f5e4f460f
giacomogroppi <giamg01@gmail.com>
2021-02-23 12:47:19 +0100

    data fix

--- src/lastedit/copy.cpp
+++ src/lastedit/copy.cpp
@@ -2,10 +2,17 @@
 
 #include "../currenttitle/checksimilecopybook.h"
 
+/*
 static void copycolor(QList<struct colore_s> *, QList<struct colore_s> *);
 static void copycoordinate(QList<double> *, QList<double> *);
 static void copypressure(QList<float> *, QList<float> *);
-static void copyidtratto(QList<int> *, QList<int> *);
+static void copyidtratto(QList<int> *, QList<int> *);*/
+
+
+static void copycolor(QList<struct point_s> *, QList<struct point_s> *);
+static void copycoordinate(QList<struct point_s> *, QList<struct point_s> *);
+static void copypressure(QList<struct point_s> *, QList<struct point_s> *);
+static void copyidtratto(QList<struct point_s> *, QList<struct point_s> *);
 
 void redoundo::copy_b(currenttitle_class *dest, currenttitle_class *src){
     int temp = COLORE;
@@ -17,23 +24,28 @@ void redoundo::copy_b(currenttitle_class *dest, currenttitle_class *src){
 
         switch (temp) {
             case COLORE:
-                copycolor(&src->datatouch->color, &dest->datatouch->color);
+                copycolor(&src->datatouch->m_point, &dest->datatouch->m_point);
+                /*copycolor(&src->datatouch->color, &dest->datatouch->color);*/
                 ok = true;
                 break;
             case XCHECK:
-                copycoordinate(&src->datatouch->x, &dest->datatouch->x);
+                copycoordinate(&src->datatouch->m_point, &dest->datatouch->m_point);
+                /*copycoordinate(&src->datatouch->x, &dest->datatouch->x);*/
                 ok = true;
                 break;
             case YCHECK:
-                copycoordinate(&src->datatouch->y, &dest->datatouch->y);
+                copycoordinate(&src->datatouch->m_point, &dest->datatouch->m_point);
+                /*copycoordinate(&src->datatouch->y, &dest->datatouch->y);*/
                 ok = true;
                 break;
             case PRESSURE:
-                copypressure(&src->datatouch->pressure, &dest->datatouch->pressure);
+                copypressure(&src->datatouch->m_point, &dest->datatouch->m_point);
+                /*copypressure(&src->datatouch->pressure, &dest->datatouch->pressure);*/
                 ok = true;
                 break;
             case IDTRATTO:
-                copyidtratto(&src->datatouch->idtratto, &dest->datatouch->idtratto);
+                copyidtratto(&src->datatouch->m_point, &dest->datatouch->m_point);
+                /*copyidtratto(&src->datatouch->idtratto, &dest->datatouch->idtratto);*/
                 ok = true;
                 break;
         }
@@ -52,8 +64,25 @@ void redoundo::copy(){
     this->indice ++;
 }
 
+static void copycolor(QList<struct point_s> *src, QList<struct point_s> *dest){
+    int i, len = src->length();
+
+
+    for(i=0; i<len; i++){
+    }
+
+}
+static void copycoordinate(QList<struct point_s> *, QList<struct point_s> *);
+static void copypressure(QList<struct point_s> *, QList<struct point_s> *);
+static void copyidtratto(QList<struct point_s> *, QList<struct point_s> *);
+
+
+/*
+ * the function is comment because it didn't work with the new datastruct
+*/
+
 /* TODO -> understand what has changed */
-void copycolor(QList<struct colore_s> *src, QList<struct colore_s> *dest){
+/*static void copycolor(QList<struct colore_s> *src, QList<struct colore_s> *dest){
     int i, len;
     len = src->length();
 
@@ -64,7 +93,7 @@ void copycolor(QList<struct colore_s> *src, QList<struct colore_s> *dest){
     }
 }
 
-void copycoordinate(QList<double> *src, QList<double> *dest){
+static void copycoordinate(QList<double> *src, QList<double> *dest){
     int i, len;
     len = src->length();
 
@@ -75,7 +104,7 @@ void copycoordinate(QList<double> *src, QList<double> *dest){
     }
 }
 
-void copypressure(QList<float> *src, QList<float> *dest){
+static void copypressure(QList<float> *src, QList<float> *dest){
     int i, len;
     len = src->length();
 
@@ -85,7 +114,7 @@ void copypressure(QList<float> *src, QList<float> *dest){
     }
 }
 
-void copyidtratto(QList<int> *src, QList<int> *dest){
+static void copyidtratto(QList<int> *src, QList<int> *dest){
     int i, len;
     len = src->length();
 
@@ -95,3 +124,4 @@ void copyidtratto(QList<int> *src, QList<int> *dest){
         dest->append(src->at(i));
     }
 }
+*/
