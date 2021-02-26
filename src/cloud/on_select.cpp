27025fa5ac45c038215e3ee17104d098c7a4ad40
giacomo groppi <giamg01@gmail.com>
2021-02-26 20:52:09 +0100

    cloud

--- src/cloud/on_select.cpp
+++ src/cloud/on_select.cpp
@@ -8,16 +8,29 @@ void cloud_ui::on_tab_main_currentChanged(int)
     this->refresh();
 }
 
+
+#define PASS_HIDE "**************"
 /*
  * temp = true -> show log in
  * temp = false -> show lon on
 */
-static void show_log_in(cloud_ui *cloud, bool temp){
+static void show_log_in(cloud_ui *cloud, bool temp, struct struct_user *user){
     if(temp)
-        cloud->ui->button_info->setText("Log in");
+        cloud->ui->button_log->setText("Log in");
     else
         cloud->ui->button_log->setText("Log on");
+
+    cloud->ui->button_pass->setHidden(temp);
+
+    if(user != NULL){
+        cloud->ui->edit_mail->setPlainText((QString)user->m_mail.m_mail);
+        cloud->ui->edit_pass->setPlainText(PASS_HIDE);
+    }
+
+    cloud->ui->edit_repeat->setHidden(!temp);
 }
+
+
 
 /*
  * redo the log in to find if the user is log
@@ -31,7 +44,7 @@ void cloud_ui::refresh(){
         if(res == n_error_cloud::password_wrong){
             dialog_critic("Your password is wronge");
 
-            show_log_in(this, true);
+            show_log_in(this, true, m_controll->m_user);
 
         }else if(res == n_error_cloud::server_down){
             dialog_critic("Unfortunately i can't connect with the server");
@@ -40,9 +53,8 @@ void cloud_ui::refresh(){
             /*
              * need to show log in button
             */
-            show_log_in(this, true);
+            show_log_in(this, true, m_controll->m_user);
 
-            /* setting balance to hide */
             ui->balance->setHidden(true);
 
         }else if(res == n_error_cloud::error_no_internet){
@@ -50,7 +62,7 @@ void cloud_ui::refresh(){
         }
 
     }else{
-        show_log_in(this, false);
+        show_log_in(this, false, m_controll->m_user);
 
         ui->balance->setText(QString::number(* (int*) pointer));
     }
