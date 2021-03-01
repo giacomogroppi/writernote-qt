0fef99d9563878acfbc669031a538a346740d95e
giacomo groppi <giamg01@gmail.com>
2021-03-01 21:29:05 +0100

    draw new page fix

--- src/touch/paintevent/disegnofoglio.cpp
+++ src/touch/paintevent/disegnofoglio.cpp
@@ -12,16 +12,17 @@
 
 static qreal widthToPressure(int);
 
-static int height_(datastruct *);
-static int width_(datastruct *);
+static double height_(datastruct *);
+static double width_(datastruct *);
+
+static void addPointZero(currenttitle_class *);
+static double getLastPoint(currenttitle_class *);
 
 void TabletCanvas::disegnafoglio(){
     if(!disegnofoglio_bool) return;
 
-    int i, last = 0;
-    int lunghezza;
-
-    int height, width;
+    double last, height, width;
+    int i;
 
     width = width_(data->datatouch);
     height = height_(data->datatouch);
@@ -29,10 +30,8 @@ void TabletCanvas::disegnafoglio(){
     if(width == INT32_MIN || height == INT32_MIN)
         return dialog_critic("We had an internal problem, restarting writer note might fix the problem");
 
-    lunghezza = (long double)NUMEROPIXELPAGINA * data->datatouch->zoom;
-
-    if(data->datatouch->posizionefoglio.length())
-       last = data->datatouch->posizionefoglio.last() + (long double)NUMEROPIXELPAGINA * data->datatouch->zoom;
+    /* he get the last point draw */
+    last = getLastPoint(data);
 
     data->datatouch->posizionefoglio.append(last);
 
@@ -47,15 +46,7 @@ void TabletCanvas::disegnafoglio(){
     struct colore_s coloretemp_struttura;
 
     /* insert a point (0, 0) */
-    if(this->data->datatouch->m_point.length() == 0){
-        struct point_s temp;
-        temp.idtratto = IDTRATTOZERO;
-        temp.m_x = 0;
-        temp.m_y = 0;
-
-        data->datatouch->m_point.append(temp);
-
-    }
+    addPointZero(data);
 
     coloretemp = setcolor(&coloretemp_struttura);
     struct point_s temp_point;
@@ -64,40 +55,40 @@ void TabletCanvas::disegnafoglio(){
 
 
 
-    int deltax = height / style->nx;
-    int deltay = width / style->ny;
+    double deltax = height / style->nx;
+    double deltay = (double)width / (double)style->ny;
 
-    /*
-     * orizzonal line
-    */
-    for(i=0; i<deltax; i++){
+    double temp = deltax;
+
+    for(i=0; i<style->nx; i++){
+        temp_point.idtratto = IDORIZZONALE;
+        temp_point.m_x = 0;
+        temp_point.m_y = last + deltax;
+
+        data->datatouch->m_point.append(temp_point);
+
+        temp_point.m_x = width;
+        data->datatouch->m_point.append(temp_point);
+
+        deltax += temp;
+    }
+
+
+    temp = deltay;
+    for(i=0; i<style->ny; i++){
+        temp_point.idtratto = IDVERTICALE;
+        temp_point.m_x = deltay;
+        temp_point.m_y = last; /* corrisponde to 0 */
+
+        data->datatouch->m_point.append(temp_point);
+
+        temp_point.m_y = height + last;
+        data->datatouch->m_point.append(temp_point);
+
+        deltay += temp;
 
     }
 
-    /*for(i = 0, last += 10; i < 61; i++, last += 40){
-        temp_point.m_x = DISTANCEFROMLEFTANDRIGHT;
-        temp_point.m_y = (double)last;
-
-        memcpy(&temp_point.m_color, &coloretemp_struttura, sizeof(struct colore_s));
-        temp_point.m_posizioneaudio = DEFAULTPOSIZIONEAUDIO;
-        temp_point.m_pressure = pressure_default;
-        temp_point.rotation = DEFAULTROTATION;
-        temp_point.idtratto = IDORIZZONALE;
-
-        data->datatouch->m_point.append(temp_point);
-
-
-        temp_point.m_x = (double)(lunghezza - DISTANCEFROMLEFTANDRIGHT);
-        temp_point.m_y = (double)last;
-
-        data->datatouch->m_point.append(temp_point);
-
-    }*/
-
-
-    /*
-     * TODO -> add x drawing
-    */
     this->disegnofoglio_bool = false;
     this->isloading = true;
 }
@@ -113,7 +104,7 @@ static inline qreal widthToPressure(int v){
  * not to break the smoothness and all the features
 */
 
-static int width_(datastruct *data){
+static double width_(datastruct *data){
     if(data->posizionefoglio.isEmpty())
         return NUMEROPIXELPAGINA;
 
@@ -125,7 +116,7 @@ static int width_(datastruct *data){
         }
     }
 
-    int temp_ = INT32_MIN;
+    double temp_ = (double)INT32_MIN;
 
     for(i=0; i<len; i++){
         if(data->m_point.at(i).m_x > temp_){
@@ -137,7 +128,7 @@ static int width_(datastruct *data){
     return temp_;
 }
 
-static int height_(datastruct *data){
+static double height_(datastruct *data){
     if(data->posizionefoglio.isEmpty())
         return NUMEROPIXELORIZZONALI;
 
@@ -146,10 +137,9 @@ static int height_(datastruct *data){
         if(data->m_point.at(i).idtratto == IDVERTICALE){
             return data->m_point.at(i+1).m_y;
         }
-
     }
 
-    int temp_ = INT32_MIN;
+    double temp_ = (double)INT32_MIN;
 
     /*
      * there are points with horizons
@@ -165,4 +155,32 @@ static int height_(datastruct *data){
     */
 
     return temp_;
+}
+
+static void addPointZero(currenttitle_class *data){
+    if(data->datatouch->m_point.length() == 0){
+        struct point_s temp;
+        temp.idtratto = IDTRATTOZERO;
+        temp.m_x = 0;
+        temp.m_y = 0;
+
+        data->datatouch->m_point.append(temp);
+    }
+}
+
+
+static double getLastPoint(currenttitle_class *data){
+    if(data->datatouch->m_point.isEmpty())
+        return (double) 0;
+
+    int i, len;
+    double p = data->datatouch->m_point.first().m_y;
+
+    len = data->datatouch->m_point.length();
+    for(i=0; i<len; i++){
+        if(data->datatouch->m_point.at(i).m_y > p)
+            p = data->datatouch->m_point.at(i).m_y;
+    }
+
+    return p;
 }
