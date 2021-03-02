68c892bb856751e5223178b641d49412250eac33
giacomo groppi <giamg01@gmail.com>
2021-03-02 12:07:45 +0100

    fix auto version on windows

--- windows/compile.sh
+++ windows/compile.sh
@@ -1,7 +1,10 @@
 mkdir build
 rm build/*
 
-qmake writernote.pro 
+version=$(git describe --tags --abbrev=0)
+## version contain the version of the last tag on github
+
+qmake writernote.pro DEFINES+=VERSION_SNAPCRAFT=$version
 make release -j $nproc 
 
 ldd ./build/writernote.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp "{}" ./build
