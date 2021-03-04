d74fd8b95c951b90260f1b833c9f99593497415c
Giacomo Groppi <giamg01@gmail.com>
2021-03-04 12:23:18 +0100

    fix windows

--- windows/compile.sh
+++ windows/compile.sh
@@ -1,9 +1,11 @@
 mkdir build
 rm build/*
 
+git pull
+
 version=$(git describe --tags --abbrev=0)
 ## version contain the version of the last tag on github
-version=1.3.15
+
 qmake writernote.pro DEFINES+=VERSION_SNAPCRAFT=$version
 make release -j $nproc 
 
