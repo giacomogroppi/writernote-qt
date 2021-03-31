4b1384755cab198cfa4944238ef958bb028df492
giacomo groppi <giamg01@gmail.com>
2021-03-31 17:17:26 +0200

    square

--- src/touch/square/size/size_square.cpp
+++ src/touch/square/size/size_square.cpp
@@ -1,20 +1,14 @@
 #include "size_square.h"
 
-
-bool size_square::isbigger(QRectF &first, QRectF &second){
+/*
+ * ritorna true se e solo se il punto più in basso a destra
+ * ha un coeffiente, tra x e y, minore del secondo
+*/
+bool size_square::isbigger(QRectF first, QRectF second){
     QPointF __first_point, __second_point;
     __first_point = first.bottomRight();
     __second_point = second.bottomRight();
 
     return (__first_point.x() < __second_point.x()
             || __first_point.y() < __second_point.y());
-}
-
-
-/*
- * ritorna true se e solo se il punto più in basso a destra
- * ha un coeffiente, tra x e y, minore del secondo
-*/
-bool size_square::isbigger(QRectF first, QRectF &second){
-    return size_square::isbigger(first, second);
 }
--- src/touch/square/size/size_square.h
+++ src/touch/square/size/size_square.h
@@ -6,9 +6,7 @@
 class size_square
 {
 public:
-    /*return true if the first one is bigger then the second one*/
-    static bool isbigger(QRectF &first, QRectF &second);
-    static bool isbigger(QRectF first, QRectF &second);
+    static bool isbigger(QRectF first, QRectF second);
 };
 
 #endif // SIZE_SQUARE_H
