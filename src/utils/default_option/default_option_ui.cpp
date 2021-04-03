30288da6c2c7b70e002e3e7ce818d42e83ee4b89
giacomo groppi <giamg01@gmail.com>
2021-04-03 23:10:04 +0200

    modificato:             src/utils/default_option/default_option_ui.cpp 	modificato:             src/utils/default_option/default_option_ui.h 	modificato:             src/utils/default_option/default_option_ui.ui

--- src/utils/default_option/default_option_ui.cpp
+++ src/utils/default_option/default_option_ui.cpp
@@ -1,14 +1,60 @@
 #include "default_option_ui.h"
 #include "ui_default_option_ui.h"
 
-default_option_ui::default_option_ui(QWidget *parent) :
+default_option_ui::default_option_ui(QWidget *parent,
+                                     QString first_button,
+                                     QString second_button) :
     QDialog(parent),
     ui(new Ui::default_option_ui)
 {
     ui->setupUi(this);
+
+    ui->first_text->setCheckable(true);
+    ui->second_text->setCheckable(true);
+
+    ui->first_text->setText(first_button);
+    ui->second_text->setText(second_button);
 }
 
 default_option_ui::~default_option_ui()
 {
     delete ui;
+}
+
+void default_option_ui::on_button_cancel_clicked()
+{
+    emit result(e_result::close);
+}
+
+void default_option_ui::closeEvent(QCloseEvent *event)
+{
+    emit result(e_result::close);
+    event->accept();
+}
+
+void default_option_ui::on_button_ok_clicked()
+{
+    if(res == 1)
+        emit result(e_result::first);
+    else if(res == 2)
+        emit result(e_result::second);
+    else
+        emit result(e_result::error);
+}
+
+void default_option_ui::on_second_text_clicked()
+{
+    res = 2;
+    updateStatus();
+}
+
+void default_option_ui::on_first_text_clicked()
+{
+    res = 1;
+    updateStatus();
+}
+
+void default_option_ui::updateStatus(){
+    ui->first_text->setChecked(res == 1);
+    ui->second_text->setChecked(res == 2);
 }
--- src/utils/default_option/default_option_ui.h
+++ src/utils/default_option/default_option_ui.h
@@ -2,6 +2,8 @@
 #define DEFAULT_OPTION_UI_H
 
 #include <QDialog>
+#include <QString>
+#include <QCloseEvent>
 
 namespace Ui {
 class default_option_ui;
@@ -12,11 +14,38 @@ class default_option_ui : public QDialog
     Q_OBJECT
 
 public:
-    explicit default_option_ui(QWidget *parent = nullptr);
+    enum e_result: int{
+        close,
+        first,
+        second,
+        error
+    };
+
+    explicit default_option_ui(QWidget *parent = nullptr,
+                               QString first_button = QString(),
+                               QString second_button = QString());
     ~default_option_ui();
 
+private slots:
+    void on_button_cancel_clicked();
+
+    void on_button_ok_clicked();
+
+    void on_second_text_clicked();
+
+    void on_first_text_clicked();
+
 private:
+    int res = -1;
+    void updateStatus();
     Ui::default_option_ui *ui;
+
+signals:
+    void result(enum e_result);
+
+protected:
+    void closeEvent(QCloseEvent *event) override;
+
 };
 
 #endif // DEFAULT_OPTION_UI_H
