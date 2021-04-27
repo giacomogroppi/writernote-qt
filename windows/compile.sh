f88bc940d3951b3c8cb15eb6bc092eae79093599
giacomogroppi <giamg01@gmail.com>
2021-04-27 17:58:38 +0200

    Update compile.sh

--- windows/compile.sh
+++ windows/compile.sh
@@ -11,4 +11,4 @@ make release -j $nproc
 
 ldd ./build/writernote.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp "{}" ./build
 
-windeployqt.exe ./build/writernote.exe --force
+/mingw64/bin/windeployqt.exe ./build/writernote.exe --force
