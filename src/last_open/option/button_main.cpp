231d8ed2640291a20c42e37a9f0027f3b492c1dd
giacomo groppi <giamg01@gmail.com>
2021-04-27 14:54:02 +0200

    modificato:             src/last_open/option/button_main.cpp

--- src/last_open/option/button_main.cpp
+++ src/last_open/option/button_main.cpp
@@ -2,16 +2,17 @@
 #include "option_last_open_ui.h"
 #include "../last_open.h"
 
-
+#define mac_ex(parent) \
+    option_last_open_ui opt(parent); \
+    opt.exec();
 
 void MainWindow::on_actionLast_open_file_triggered()
 {
-
+    mac_ex(nullptr);
 
 }
 
 void last_open::on_option_button_clicked()
 {
-    option_last_open_ui opt(nullptr);
-    opt.exec();
+    mac_ex(nullptr);
 }
