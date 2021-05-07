00822d52531c3592a33ba5c43b639ad5353352e1
Giacomo Groppi <giamg01@gmail.com>
2021-05-07 06:35:33 +0200

    windows build for updater

--- windows/compile.sh
+++ windows/compile.sh
@@ -16,5 +16,6 @@ ldd ./build/writernote.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp 
 cd updater
 /mingw64/bin/qmake updater.pro
 make release -j $nproc
-/mingw64/bin/windeployqt.exe ./updater.exe --force
+ldd ./release/writernote.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp "{}" ./release
+/mingw64/bin/windeployqt.exe release/updater.exe --force
 
