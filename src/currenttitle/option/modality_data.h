3056fd7c06b5ec33560bbb7a817b9d77fa32cddf
Giacomo Groppi <giamg01@gmail.com>
2021-06-19 09:03:35 +0200

    nuovo file:             modality_data.h

---
+++ src/currenttitle/option/modality_data.h
@@ -0,0 +1,20 @@
+#ifndef MODALITY_DATA_H
+#define MODALITY_DATA_H
+
+#include "../../restore_file/ui/setting_restore_ui.h"
+
+#include "QtGlobal"
+
+#define LEN_MODAL 20
+#define MOD_SAVE_ALL 1
+#define MOD_ASK_SAVE 2
+#define MOD_NOT_SAVE 3
+
+struct modality{
+    char name[LEN_MODAL];
+    struct __data m_data;
+
+    uchar save_when_change = MOD_ASK_SAVE;
+};
+
+#endif // MODALITY_DATA_H
