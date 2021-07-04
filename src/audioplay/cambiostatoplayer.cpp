631eb2ba6a2fd0c49476585f5aea87bef0535d95
Giacomo Groppi <giamg01@gmail.com>
2021-07-04 21:32:26 +0200

    modificato:             src/audioplay/cambiostatoplayer.cpp

--- src/audioplay/cambiostatoplayer.cpp
+++ src/audioplay/cambiostatoplayer.cpp
@@ -5,12 +5,13 @@
 #include "cambioiconariascolto.h"
 
 void MainWindow::cambiostatoplayer(QMediaPlayer::State state){
-    const bool s = state == QMediaPlayer::StoppedState;
+    const bool s = (state == QMediaPlayer::StoppedState);
 
-    this->ui->stop_play->setHidden(!s);
+    ui->stop_play->setHidden(s);
+    ui->start_play->setHidden(s);
 
-    this->ui->volumeSlider->setHidden(!s);
-    this->ui->audioSlider->setHidden(!s);
+    ui->volumeSlider->setHidden(s);
+    ui->audioSlider->setHidden(s);
 
     if(s){
         ui->volumeSlider->setValue(0);
@@ -18,37 +19,37 @@ void MainWindow::cambiostatoplayer(QMediaPlayer::State state){
     }
 
     ui->listWidgetSX->setEnabled(s);
-    this->ui->insertimagebotton->setEnabled(s);
+    ui->insertimagebotton->setEnabled(s);
 
-    this->ui->actionImport_Video->setEnabled(s);
-    this->ui->actionDelete_audio->setEnabled(s);
-    this->ui->actionDelete_copybook->setEnabled(s);
+    ui->actionImport_Video->setEnabled(s);
+    ui->actionDelete_audio->setEnabled(s);
+    ui->actionDelete_copybook->setEnabled(s);
 
     /* creazione del nuovo copybook */
-    this->ui->actionCreate_new_copybook->setEnabled(s);
-    this->ui->actionListen_current_audio->setChecked(!s);
+    ui->actionCreate_new_copybook->setEnabled(s);
+    ui->actionListen_current_audio->setChecked(!s);
     /* cambiare il font in grandezza e in tipo*/
-    this->ui->spinBox_fontsize->setEnabled(s);
-    this->ui->fontComboBox_fonttipo->setEnabled(s);
-    this->ui->inserttablebotton->setEnabled(s);
-    this->ui->listbotton->setEnabled(s);
-    this->ui->boldbotton->setEnabled(s);
+    ui->spinBox_fontsize->setEnabled(s);
+    ui->fontComboBox_fonttipo->setEnabled(s);
+    ui->inserttablebotton->setEnabled(s);
+    ui->listbotton->setEnabled(s);
+    ui->boldbotton->setEnabled(s);
 
     /* l'utente ha deciso di stoppare l'audio oppure è finito */
     if(state == QMediaPlayer::StoppedState){
 
-        this->ui->start_rec->setEnabled(false);
+        ui->start_rec->setEnabled(false);
 
         QIcon icon;
         icon.addFile(QString::fromUtf8(":image/images/play.png"), QSize(), QIcon::Normal, QIcon::Off);
-        this->ui->start_play->setIcon(icon);
+        ui->start_play->setIcon(icon);
 
-        this->ui->statusBar->clearMessage();
+        ui->statusBar->clearMessage();
 
-        this->ui->textEdit->setHtml(this->m_currenttitle->testi);
+        ui->textEdit->setHtml(this->m_currenttitle->testi);
 
         m_canvas->m_pos_ris = -1;
     }
 
-    this->ui->start_rec->setEnabled(false);
+    ui->start_rec->setEnabled(false);
 }
