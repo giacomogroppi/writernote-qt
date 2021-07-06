df4d5cde23b16e71bc675dbef76ad100bf2995b3
Giacomo Groppi <giamg01@gmail.com>
2021-07-06 18:13:26 +0200

    nuovo file:             script.py

---
+++ script.py
@@ -0,0 +1,12 @@
+import os
+
+def find_files(filename, search_path):
+   result = []
+
+# Wlaking top-down from the root
+   for root, dir, files in os.walk(search_path):
+      if filename in files:
+         result.append(os.path.join(root, filename))
+   return result
+
+print("Trovato: ",find_files("mt.exe","C:"))
