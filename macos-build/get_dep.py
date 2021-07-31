4cb823124bbc01e06a2c19a09fd961f0218cafed
giacomogroppi <giamg01@gmail.com>
2021-07-31 09:02:05 +0200

    Update get_dep.py

--- macos-build/get_dep.py
+++ macos-build/get_dep.py
@@ -33,7 +33,7 @@ def get_dep(pos_binary: str, dest_list: str) -> list[str]:
         for line in lines:
             list.append(line)
 
-    list = list[1:]
+    del list[0]
     print("list from get dep:", list)
     list_sec = []
     i = 0
