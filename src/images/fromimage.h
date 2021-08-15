f7ac54c8da2c3a33aa0aaa738b4cd3ca6d0bbc2e
giacomo groppi <giamg01@gmail.com>
2021-08-15 15:19:43 +0200

    draw image

--- src/images/fromimage.h
+++ src/images/fromimage.h
@@ -7,8 +7,10 @@
 #include "../touch/datastruct/datastruct.h"
 #include <QPointF>
 #include <QImage>
+#include <QPainter>
 
 #define delta_point 200
+#define SUFFIX_IMG "_img_"
 
 class Document;
 struct immagine_S{
@@ -24,8 +26,12 @@ private:
     QList<struct immagine_S> m_img;
     inline QStringList get_name_img();
 
+    static inline QString getNameNoCopy(const uint i){
+        return SUFFIX_IMG + QString::number(uint(i));
+    }
+
     static inline QString getName(const QString &p, const uint i){
-        return p + "_img_" + QString::number(uint(i));
+        return p + fromimage::getNameNoCopy(i);
     }
 
     immagine_S *insert_image(const char *__pos,
@@ -56,8 +62,27 @@ public:
     inline void draw(QPainter &painter,
                      const uint pwidth,
                      const int rend_width,
-                     const int rend_heigth){
+                     const int rend_heigth) const{
+        uint i, len;
+        QRectF rect;
 
+        len = this->m_img.length();
+        for(i=0; i<len; ++i){
+            const struct immagine_S &img = m_img.at(i);
+            if(img.f.y() < 0)
+                continue;
+            if(img.f.x() < 0)
+                continue;
+            if(img.i.y() > rend_width)
+                continue;
+            if(img.i.x() > rend_heigth)
+                continue;
+
+            rect = QRectF(img.i, img.f);
+
+            painter.drawImage(rect, img.immagini, rect);
+
+        }
     }
 
     void reset(){
