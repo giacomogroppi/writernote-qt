903ae25a514c07e7b64b91f3062ec478560e93cd
Giacomo Groppi <giamg01@gmail.com>
2021-09-26 12:22:26 +0200

    ios build

--- src/videocompress/compressivideo.cpp
+++ src/videocompress/compressivideo.cpp
@@ -1,11 +1,16 @@
 #include "compressivideo.h"
+#include "../utils/dialog_critic/dialog_critic.h"
 
 #include <stdlib.h>
 
 int compressvideo(QString *posizionefrom, QString *to, int fps, QString compressione)
 {
+#if defined(ANDROID_WRITERNOTE) || defined(IOS_WRITERNOTE)
+    user_message("Video compress is not available on your platform");
+#else
     /* ffmpeg -i input.mp4 -vcodec libx265 -crf 28 output.mp4*/
     QString comando = "ffmpeg -i " + *posizionefrom +  " -vcodec " + compressione + " -crf " + QString::number(fps) + " " + *to;
 
     return system(comando.toUtf8().constData());
+#endif
 }
