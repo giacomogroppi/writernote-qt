50c13c28f2c7a8c582cfe45cf8161580b00edbbe
Giacomo Groppi <giamg01@gmail.com>
2021-10-09 16:59:12 +0200

    windows fix

--- src/utils/path/pathfile.cpp
+++ src/utils/path/pathfile.cpp
@@ -1,32 +1,28 @@
 #include "pathfile.h"
 #include "../slash/slash.h"
 
-
-
 QString pathFile::remove_file(const QString &path)
 {
     const char slash = slash::__slash();
     const int indexLast = path.lastIndexOf(slash);
 
 #if defined(WIN32) || defined(WIN64) || defined(__OS2__)
-    if(indexLast == -1){
-        return changeSlash(path.mid(0, path.lastIndexOf('/')));
-    }
-#endif
+    return changeSlash(path.mid(0, path.lastIndexOf('/')));
+#else
     return path.mid(0, indexLast);
+#endif
 }
 
+#if defined(WIN32) || defined(WIN64) || defined(__OS2__)
 QString pathFile::changeSlash(const QString &path){
     QString tmp = path;
-    int i, len;
-    len = path.length();
-
-#if defined(WIN32) || defined(WIN64) || defined(__OS2__)
+    int i;
+    const int len = path.length();
     for(i=0; i<len; ++i){
         if(tmp.at(i) == '/')
             tmp.operator[](i) = '\\';
     }
-#endif
 
     return tmp;
 }
+#endif
--- src/utils/path/pathfile.h
+++ src/utils/path/pathfile.h
@@ -7,7 +7,10 @@ class pathFile
 {
 public:
     static QString remove_file(const QString &path);
+
+#if defined(WIN32) || defined(WIN64) || defined(__OS2__)
     static QString changeSlash(const QString &path);
+#endif
 };
 
 #endif // PATHFILE_H
