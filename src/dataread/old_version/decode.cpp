5c8311ada7d1310c2f0bb7edd719abcc3082954f
Giacomo Groppi <giamg01@gmail.com>
2021-10-24 15:29:57 +0200

    modificato:             CMakeLists.txt 	modificato:             src/dataread/old_version/decode.cpp

--- src/dataread/old_version/decode.cpp
+++ src/dataread/old_version/decode.cpp
@@ -4,11 +4,11 @@
 static void scaleAll(QList<point_s> &point, const QPointF &translation);
 
 void xmlstruct::decode(Document *data, QList<point_s> &point, QList<double> pos_foglio){
+    const point_s firstPoint = point.takeFirst();
     const uint lenList = point.length();
     const uint lenPage = pos_foglio.length();
-    const auto firstPoint = point.takeFirst();
+    const point_s *pointAppend;
     uint i, counterPage;
-
     const QPointF translation(firstPoint.m_x, firstPoint.m_y);
     const uint height = page::getHeight();
 
@@ -17,20 +17,20 @@ void xmlstruct::decode(Document *data, QList<point_s> &point, QList<double> pos_
 
     for(counterPage = 0; counterPage < lenPage ; counterPage ++){
         data->datatouch->newPage(n_style::white);
-        for(i=0; i<point.length(); ++i){
-            const auto &ref = point.at(i);
-            if(ref.m_y > counterPage*height  &&  ref.m_y <= (counterPage+1)*height)
-                data->datatouch->at_mod(counterPage)->append(ref);
+        for(i=0; i<lenList; ++i){
+            pointAppend = &point.at(i);
+            if(pointAppend->m_y > counterPage*height  &&  pointAppend->m_y <= (counterPage+1)*height)
+                data->datatouch->at_mod(counterPage)->append(pointAppend);
         }
     }
-
 }
 
 static void scaleAll(QList<point_s> &point, const QPointF &translation){
     uint i;
     const uint len = point.length();
+    point_s *ref;
     for(i=0; i<len; i++){
-        point_s *ref = &point.operator[](i);
+        ref = &point.operator[](i);
         ref->m_x -= translation.x();
         ref->m_y -= translation.y();
     }
