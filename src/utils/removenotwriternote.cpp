ddc5a28b7cd408388775d4a93eb9d666daa8c34a
Giacomo Groppi <giamg01@gmail.com>
2021-11-19 22:30:51 +0100

    fix missing removing

--- src/utils/removenotwriternote.cpp
+++ src/utils/removenotwriternote.cpp
@@ -4,7 +4,9 @@ void removeNotWriternote::exe(QStringList &ref)
 {
     int i;
     for(i=0; i<ref.length(); ++i){
-        if(ref.at(i).indexOf('.' + APP_EXT) == -1)
+        if(ref.at(i).indexOf('.' + APP_EXT) == -1){
             ref.removeAt(i);
+            i--;
+        }
     }
 }
