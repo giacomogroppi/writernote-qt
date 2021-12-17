56f410428c4edf98187acb59bc4f61e3d8484176
giacomo groppi <giamg01@gmail.com>
2021-12-17 10:46:28 +0100

    common script

--- src/utils/common_script.h
+++ src/utils/common_script.h
@@ -57,7 +57,7 @@ Q_ALWAYS_INLINE int order(QList<T> &list)
     for (i = 0, mod = 0; i < n - 1; i++){
         for (j = 0; j < n - i - 1; j++){
             if (list[j] > list[j+1]){
-                __swap(&list[j], &list[j+1]);
+                list.swapItemsAt(j, j + 1);
                 mod = 1;
             }
         }
