7a32023820f31a8ce3896e4c5fbb75177ff6966c
giacomo groppi <giamg01@gmail.com>
2021-12-19 11:34:52 +0100

    windows: fix: updater

--- updater/launch_updater/launch_updater.cpp
+++ updater/launch_updater/launch_updater.cpp
@@ -1,11 +1,18 @@
 #include "launch_updater.h"
 #include "stdlib.h"
+#include "src/utils/dialog_critic/dialog_critic.h"
+#include "src/utils/mostra_explorer/mostra_explorer.h"
 
 void launch_updater::launch()
 {
 #if defined(WIN64) || defined(WIN32)
     int res;
-    res = system("C:\\Program Files (x86)\\writernote\\updater.exe");
+    res = system("C:\\'Program Files (x86)'\\writernote\\updater.exe");
+
+    if(res){
+        user_message("It was not possible to open the updater, if you want to update writernote, open the program called updater.exe");
+        mostra_explorer("C:\\Program Files (x86)\\writernote\\");
+    }
 
 #endif
 
