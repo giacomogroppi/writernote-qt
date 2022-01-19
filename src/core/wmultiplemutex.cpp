0f7c504ea1eace230e19cb63e97f07fadcc65f99
Giacomo Groppi <giamg01@gmail.com>
2022-01-19 23:03:08 +0100

    fix: multi-mutex

--- src/core/wmultiplemutex.cpp
+++ src/core/wmultiplemutex.cpp
@@ -1,52 +1,38 @@
 #include "wmultiplemutex.h"
 #include "utils/common_script.h"
 
-WMultipleMutex::WMultipleMutex(int maxVal, int initVal)
+WMultipleMutex::WMultipleMutex(int size, int initVal)
 {
     int i;
     W_ASSERT(initVal >= 0);
-    W_ASSERT(initVal < maxVal);
+    W_ASSERT(initVal < size);
 
-    this->maxVal = maxVal;
-    this->mutex = (pthread_mutex_t *)malloc(sizeof(*mutex) * (maxVal + 1));
+    this->size = size;
+    this->sem = (sem_t *)malloc(sizeof(*sem) * (size + 1));
 
-    for(i = 0; i < maxVal; i ++){
-        pthread_mutex_init(&mutex[i], NULL);
-    }
-
-    for(i = maxVal; i >= initVal; i--){
-        qDebug() << i;
-        pthread_mutex_lock(&mutex[i]);
+    // it already set all sem to 0 (lock)
+    for(i = 0; i < size; i ++){
+        sem_init(&sem[i], 0, 0);
     }
 }
 
 WMultipleMutex::~WMultipleMutex()
 {
-    free(mutex);
+    int i;
+    for(i = 0; i < size; i++)
+        sem_destroy(&sem[i]);
+
+    free(sem);
+
+    sem = 0;
 }
 
 int WMultipleMutex::lock(int num)
 {
-    W_ASSERT(num < maxVal);
+    W_ASSERT(num < size);
     W_ASSERT(num >= 0);
 
-    return pthread_mutex_lock(&mutex[num]);
+    return this->block(num, num + 1);
 }
 
-// it means that the last thread is trying to free
-// the last one its successes, which does not
-// exist, so we return only 0
-int WMultipleMutex::unlock(int n)
-{
-    W_ASSERT(n <= maxVal);
-    W_ASSERT(n >= 0);
 
-    // it means that the last thread is trying to free
-    // the last one its successes, which does not exist,
-    // so we return only 0, or the first thread is
-    // trying to authorize its previous one
-    if(unlikely(n == maxVal || n == -1))
-        return 0;
-
-    return pthread_mutex_unlock(&mutex[n]);
-}
--- src/core/wmultiplemutex.h
+++ src/core/wmultiplemutex.h
@@ -8,24 +8,58 @@
 class WMultipleMutex
 {
 private:
-    pthread_mutex_t *mutex;
-    int maxVal;
+    sem_t *sem;
+    int size;
 public:
     WMultipleMutex(int maxVal, int initVal);
     ~WMultipleMutex();
 
-    void blockAll();
+    // to is not included
+    int block(int from, int to);
+    int blockAll();
+
+    int unlock(int from, int to);
 
     int lock(int n);
     int unlock(int n);
 };
 
-force_inline void WMultipleMutex::blockAll()
+
+#define WMULTIMUTEX_ALREADY_LOCK 1
+force_inline int WMultipleMutex::blockAll()
 {
-    int i = maxVal;
-    for(i--; i >= 0; i--){
-        pthread_mutex_lock(&mutex[i]);
+    return this->block(0, size - 1);
+}
+
+force_inline int WMultipleMutex::block(int from, int to)
+{
+    for(; from < to; from ++){
+        if(unlikely(sem_trywait(&sem[to]))){
+            return -WMULTIMUTEX_ALREADY_LOCK;
+        }
     }
+
+    return 0;
+}
+
+force_inline int WMultipleMutex::unlock(int from, int to)
+{
+    W_ASSERT(to <= size);
+    W_ASSERT(from >= 0);
+
+    for(; from < to; from ++){
+        sem_post(&sem[from]);
+    }
+
+    return 0;
+}
+
+// it means that the last thread is trying to free
+// the last one its successes, which does not
+// exist, so we return only 0
+force_inline int WMultipleMutex::unlock(int n)
+{
+    return this->unlock(n, n+1);
 }
 
 #endif // WMULTIPLEMUTEX_H
