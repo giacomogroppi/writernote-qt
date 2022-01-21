f745693a2bd8ab9449a3e1a60aa1cc6379d84505
Giacomo Groppi <giamg01@gmail.com>
2022-01-21 22:19:22 +0100

    fix: read file from disk

--- src/dataread/load_from_file.cpp
+++ src/dataread/load_from_file.cpp
@@ -1,43 +1,21 @@
 #include "load_from_file.h"
-
+#include "utils/common_script.h"
+#include <QFile>
 
 bool load_from_file::exe(QByteArray &arr, const QString &path, const bool clear)
 {
-    FILE *fp;
-    uchar __read;
-    uchar size;
-    bool error = true;
+    QFile file(path);
 
     if(clear){
         arr.clear();
     }
 
-#if defined(WIN32) || defined(WIN64)
-    fp = fopen(path.toUtf8().constData(), "rb");
-#else
-    fp = fopen(path.toUtf8().constData(), "r");
-#endif
-
-    if(!fp){
+    if(unlikely(file.open(QIODevice::ReadOnly))){
         return false;
     }
 
-    while(1){
-        size = fread(&__read, 1, 1, fp);
+    arr = file.readAll();
 
-        if(feof(fp))
-            break;
-
-        if(!size){
-            error = false;
-            goto free;
-        }
-
-        arr.append(__read);
-    }
-
-
-    free:
-    fclose(fp);
-    return error;
+    file.close();
+    return true;
 }
