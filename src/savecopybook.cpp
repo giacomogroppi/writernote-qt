156cc5aaa1899adb40708b7bc2d9bf7f1635adc0
Giacomo Groppi <giamg01@gmail.com>
2022-01-27 14:14:07 +0100

    modificato:             src/savecopybook.cpp

--- src/savecopybook.cpp
+++ src/savecopybook.cpp
@@ -16,34 +16,46 @@ savecopybook::savecopybook(MainWindow *parent_U){
 }
 
 /* funzione che gestisce il salvataggio del copybook [chiedendo all'utente se vuole salvare il file o no]*/
-bool savecopybook::check_permission(){
+bool savecopybook::check_permission()
+{
     QMessageBox msgBox;
+    int ret;
+    savefile save_class(&this->parent->m_path, this->parent->m_canvas->data);
+    QString SaveMessage;
+
     msgBox.setText("The document has been modified.");
-    msgBox.setInformativeText("Do you want to save your changes?");
+
+    if(parent->m_path.isEmpty()){
+        SaveMessage = "Do you want to save your changes?";
+    }else{
+        SaveMessage = QString("Do you want to save your changes in ?").arg(parent->m_path);
+    }
+
+    msgBox.setInformativeText(SaveMessage);
+
     msgBox.setStandardButtons(QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
     msgBox.setDefaultButton(QMessageBox::Save);
-    int ret = msgBox.exec();
+    ret = msgBox.exec();
 
-    if(ret == QMessageBox::Discard)
+    if(ret == QMessageBox::Discard){
         /*close without saving*/
         return true;
-    if(ret == QMessageBox::Cancel)
+    }
+    if(ret == QMessageBox::Cancel){
         /*cancel*/
         return false;
+    }
 
     /* else save*/
-    if(parent->m_path == "")
-    {
+    if(parent->m_path.isEmpty()){
         if(!qfilechoose::getFileForSave(parent->m_path, TYPEFILEWRITER))
             return false;
     }
-    savefile save_class(&this->parent->m_path, this->parent->m_canvas->data);
 
-    const bool check = save_class.savefile_check_file() == OK;
-    if(!check)
+    ret = save_class.savefile_check_file() == OK;
+    if(!ret)
         dialog_critic("We had a problem saving the copybook");
 
-
-    return check;
+    return ret;
 }
 
