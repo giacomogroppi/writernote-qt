6044c9dc24011f2d5f22f286f8fa3c337853639f
Giacomo Groppi <giamg01@gmail.com>
2022-01-27 14:14:21 +0100

    modificato:             src/utils/areyousure/areyousure.h

--- src/utils/areyousure/areyousure.h
+++ src/utils/areyousure/areyousure.h
@@ -5,11 +5,12 @@
 #include <QDialog>
 #include <QMessageBox>
 
-inline bool areyousure(const QString &title, const QString &body){
-    QMessageBox::StandardButton resBtn = QMessageBox::question( nullptr, title,
-                                                                body,
-                                                                QMessageBox::Cancel | QMessageBox::No | QMessageBox::Yes,
-                                                                QMessageBox::Yes);
+inline bool areyousure(const QString &title, const QString &body)
+{
+    QMessageBox::StandardButton resBtn;
+    resBtn = QMessageBox::question( nullptr, title, body,
+                                    QMessageBox::Cancel | QMessageBox::No | QMessageBox::Yes,
+                                    QMessageBox::Yes);
 
     if(resBtn == QMessageBox::Yes)
         return true;
