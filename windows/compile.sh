96bf617f6f81160bb2bfcc0976b7522a5702e95b
giacomogroppi <giamg01@gmail.com>
2022-03-14 00:08:28 +0100

    Update compile.sh

--- windows/compile.sh
+++ windows/compile.sh
@@ -7,7 +7,7 @@ version=$(git describe --tags --abbrev=0)
 ## version contain the version of the last tag on github
 
 /mingw64/bin/qmake writernote.pro DEFINES+="VERSION_SNAPCRAFT=$version" DEFINES+=QT_NO_DEBUG_OUTPUT
-make release -j $nproc 
+make release -j 2
 
 echo "INFO WRITERNOTE: Add library"
 ldd ./build/writernote.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp "{}" ./build
@@ -18,7 +18,7 @@ echo "INFO WRITERNOTE: exec windeployqt"
 echo "INFO WRITERNOTE: Build updater"
 cd updater
 /mingw64/bin/qmake updater.pro
-make release -j $nproc
+make release -j 2
 ldd ./build/updater.exe | grep '\/mingw.*\.dll' -o | sort -u | xargs -I{} cp "{}" ./build
 /mingw64/bin/windeployqt.exe build/updater.exe --force
 
