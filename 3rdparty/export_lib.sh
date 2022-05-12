85a34e99149e935e46d9d4c8a7bcf8bbef2f2860
Giacomo Groppi <giamg01@gmail.com>
2022-05-12 10:42:33 +0200

    fix: macos build

--- 3rdparty/export_lib.sh
+++ 3rdparty/export_lib.sh
@@ -1,5 +1,4 @@
 echo "Export lib for writernote. pwd:" $(pwd)
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/writernote-qt/3rdparty/poppler/install
-export LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/writernote-qt/3rdparty/poppler/install
 echo $LD_LIBRARY_PATH
 
