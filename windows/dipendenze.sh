f5e77acc3c9a8458701a2d7b63cdc8a492b82dbd
Giacomo Groppi <giamg01@gmail.com>
2022-05-13 10:12:55 +0200

    Update dipendenze.sh

--- windows/dipendenze.sh
+++ windows/dipendenze.sh
@@ -1,17 +1,14 @@
 pacman --noconfirm -Sy
 
-pacman --noconfirm -S git
-pacman --noconfirm -S mingw-w64-x86_64-toolchain \
+pacman --noconfirm -S git \
+          mingw-w64-x86_64-toolchain \
           mingw-w64-x86_64-cmake \
           patch \
           mingw-w64-x86_64-cppunit \
-          make
-
-pacman --noconfirm -S mingw-w64-x86_64-nsis
-
-pacman --noconfirm -S mingw-w64-x86_64-qt6-base \
+          make \
+          mingw-w64-x86_64-nsis \
+          mingw-w64-x86_64-qt6-base \
           mingw-w64-x86_64-libzip \
-          mingw-w64-x86_64-glib2
-
-pacman --noconfirm -S mingw-w64-x86_64-poppler-qt6
-pacman --noconfirm -S mingw-w64-x86_64-ninja
+          mingw-w64-x86_64-glib2 \
+          mingw-w64-x86_64-poppler-qt6 \
+          mingw-w64-x86_64-ninja
