43c52680d87e4828a53c1f64d02c5d5766c3fc53
giacomogroppi <giamg01@gmail.com>
2022-05-13 22:10:36 +0200

    fix: macos build

--- windows/dipendenze.sh
+++ windows/dipendenze.sh
@@ -11,4 +11,5 @@ pacman --noconfirm -S git \
           mingw-w64-x86_64-poppler-qt6 \
           mingw-w64-x86_64-ninja \
           mingw-w64-x86_64-qt6-multimedia \
-          mingw-w64-x86_64-boost
+          mingw-w64-x86_64-boost \
+          mingw-w64-x86_64-openssl
