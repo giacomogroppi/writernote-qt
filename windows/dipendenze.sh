32dee11e61f0cdf328838ab9232350e8b8e6f1df
giacomo groppi <giamg01@gmail.com>
2022-05-13 11:14:01 +0200

    windows: porting to qt6

--- windows/dipendenze.sh
+++ windows/dipendenze.sh
@@ -12,5 +12,5 @@ pacman --noconfirm -S git \
           mingw-w64-x86_64-glib2 \
           mingw-w64-x86_64-poppler-qt6 \
           mingw-w64-x86_64-ninja \
-          mingw-w64-x86_64-qt6-multimedia
+          mingw-w64-x86_64-qt6-multimedia \
           mingw-w64-x86_64-boost
