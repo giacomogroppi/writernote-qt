1263e334db2d43f0c9f67eec4764fdb397b01534
Giacomo Groppi <giamg01@gmail.com>
2023-01-08 23:56:59 +0100

    fix: mozjpeg build [macos]

--- macos-build/build_mozjpeg.sh
+++ macos-build/build_mozjpeg.sh
@@ -1,13 +1,24 @@
 cd $PREFIX_LIB
-cd mozjpeg
+
+sudo rm -r mozjpeg
 
 git clone -b v4.0.1-rc2 https://github.com/mozilla/mozjpeg.git
 
-cmake -B build . \
+cd mozjpeg
+
+mkdir build
+mkdir install
+
+cd build
+
+cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_INSTALL_PREFIX=$PREFIX_LIB/mozjpeg \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX_LIB/mozjpeg/install \
   -DWITH_JPEG8=ON \
   -DPNG_SUPPORTED=OFF
 
-cmake --build build -j$(nproc)
-sudo cmake --install build
+make -j$(nproc)
+sudo make install
+
+#cmake --build . -j$(nproc)
+#sudo cmake --install build
