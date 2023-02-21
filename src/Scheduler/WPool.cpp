223f4ba6ef9a66b53e93d5c096a30c258935ff59
Giacomo Groppi <giamg01@gmail.com>
2023-02-21 13:54:17 +0100

    Update WPool.cpp

--- src/Scheduler/WPool.cpp
+++ src/Scheduler/WPool.cpp
@@ -13,4 +13,16 @@ void WPool::setPriority(int priority)
 {
     W_ASSERT(priority >= 0);
     this->_priority = priority;
+    emit this->priorityChanged();
+}
+
+void WPool::startJobs(QThreadPool *pool)
+{
+    W_ASSERT(pool != nullptr);
+    W_ASSERT(this->_active_thread.value() == 0);
+    for (auto &task : this->_tasks) {
+        pool->start(task);
+    }
+
+    _active_thread = static_cast<int>(this->_tasks.size());
 }
