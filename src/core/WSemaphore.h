7387aa2a5c0f119639ac46bb01f77199a442959d
Giacomo Groppi <giamg01@gmail.com>
2023-03-31 22:47:28 +0200

    fix: semaphore on ios

--- src/core/WSemaphore.h
+++ src/core/WSemaphore.h
@@ -2,23 +2,11 @@
 
 #include "utils/WCommonScript.h"
 
-#if defined(Q_OS_MAC) || defined(Q_OS_IOS)
-# define SEM_UNIX
-#endif
-
-#ifdef SEM_UNIX
-# include <semaphore>
-#else
-# include "semaphore.h"
-#endif
+#include <QSemaphore>
 
 class WSemaphore {
 private:
-#ifdef SEM_UNIX
-    std::counting_semaphore<> _sem;
-#else
-    sem_t _sem;
-#endif
+    QSemaphore _sem;
 public:
     explicit WSemaphore(int init_value = 0);
     ~WSemaphore();
@@ -29,45 +17,23 @@ public:
 };
 
 inline WSemaphore::WSemaphore(int init_value)
-#ifdef SEM_UNIX
     : _sem(init_value)
-#endif
 {
-#ifndef SEM_UNIX
-    sem_init(&_sem, 0, init_value);
-#endif
 }
 
-inline WSemaphore::~WSemaphore()
-{
-#ifndef SEM_UNIX
-    sem_destroy(&_sem);
-#endif
-}
+inline WSemaphore::~WSemaphore() = default;
 
 inline void WSemaphore::acquire()
 {
-#ifdef SEM_UNIX
     _sem.acquire();
-#else
-    sem_wait(&_sem);
-#endif
 }
 
 inline void WSemaphore::release()
 {
-#ifdef SEM_UNIX
     _sem.release();
-#else
-    sem_post(&_sem);
-#endif
 }
 
 inline bool WSemaphore::tryWait()
 {
-#ifdef SEM_UNIX
     return this->_sem.try_acquire();
-#else
-    return sem_trywait(&_sem) == 0;
-#endif
 }
--- src/videocompress/compressivideo.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <QString>
-
-/* funzione che si occupa di gestire la compressione del video vera e propria
-fps intero
-*/
-int compressvideo(QString *posizionefrom, QString *to, int fps, QString compressione);
