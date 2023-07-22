dc9d504b636352a4eaf2f69ec366afe8344d0499
Giacomo Groppi <giamg01@gmail.com>
2023-07-22 08:40:13 +0200

    Add option to pass a compare custom function to binary_search method

--- src/core/WAbstractList.h
+++ src/core/WAbstractList.h
@@ -1,5 +1,4 @@
-#ifndef WRITERNOTE_WABSTRACTLIST_H
-#define WRITERNOTE_WABSTRACTLIST_H
+#pragma once
 
 #include "utils/WCommonScript.h"
 #include <algorithm>
@@ -32,11 +31,18 @@ namespace WAbstractList {
      * \param begin The begin iterator
      * \param end The end iterator
      * \return end iff object is not between [begin, end)
+     * \param cmp needs to return true iff v1 >= v2
      * */
     template <class Iterator, class T>
-    auto binary_search(Iterator begin, Iterator end, const T& object) -> Iterator
+    auto binary_search(
+            Iterator begin,
+            Iterator end,
+            const T& object,
+            auto cmp = [](const T& v1, const T& v2) {
+                return v1 >= v2;
+            }
+        ) -> Iterator
     {
-        
+        static_assert(0);
     }
 };
-#endif //WRITERNOTE_WABSTRACTLIST_H
