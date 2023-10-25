cc31339cbea18e9fddf2e039dbb312c33f3cc790
Giacomo Groppi <giamg01@gmail.com>
2023-10-25 23:21:18 +0200

    Add DebugVariable that automatically disable itself in release mode

---
+++ src/core/DebugVariable.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "utils/WCommonScript.h"
+
+template <class T>
+class DebugVariable
+{
+    static_assert(std::is_arithmetic<T>::value);
+    T _value;
+
+public:
+    DebugVariable() = default;
+    DebugVariable(T value) : _value(value) {};
+
+    auto operator++() -> T&;
+    auto operator--() -> T&;
+
+    auto operator+=(T amount) -> T&;
+    auto operator-=(T amount) -> T&;
+    auto operator*=(T amount) -> T&;
+    auto operator/=(T amount) -> T&;
+
+    operator T() const;
+
+    auto operator=(T value) -> DebugVariable&;
+};
+
+#ifdef DEBUGINFO
+template <class T>
+auto DebugVariable<T>::operator++() -> T&
+{
+    this->_value ++;
+}
+
+template <class T>
+auto DebugVariable<T>::operator--() -> T&
+{
+    this->value --;
+}
+
+template <class T>
+auto DebugVariable<T>::operator+=(T amount) -> T&
+{
+    this->_value += amount;
+}
+
+template <class T>
+auto DebugVariable<T>::operator-=(T amount) -> T&
+{
+    this->_value -= amount;
+}
+
+template <class T>
+auto DebugVariable<T>::operator*=(T amount) -> T&
+{
+    this->_value *= amount;
+}
+
+template <class T>
+auto DebugVariable<T>::operator/=(T amount) -> T&
+{
+    this->_value /= amount;
+}
+
+template <class T>
+DebugVariable<T>::operator T() const
+{
+    return this->_value;
+}
+
+template <class T>
+auto DebugVariable<T>::operator=(T value) -> DebugVariable&
+{
+    this->_value = value;
+    return *this;
+}
+
+
+#endif // DEBUGINFO
+
+// TODO: implement method in non debug (all empty)
+
